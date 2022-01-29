f435ed108f7cc25891fc35341f559c8ee57a15ca
Daniel "soro" Cunha <danielsoro@gmail.com>
2022-01-29 12:49:04 -0300

    Update entries and mix format

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -39,13 +39,62 @@ defmodule TodoList do
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
+  @spec update_entry(%TodoList{auto_id: number, entries: map}, %{
+          :id => number,
+          optional(any) => any
+        }) :: %TodoList{auto_id: number, entries: map}
+  @doc """
+  Update entry based in a new entry
+
+  ## Examples
+
+      iex> TodoList.update_entry(%TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}, %{id: 1, date: ~D[2018-12-20], title: "Appoitment"})
+      %TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2018-12-20], title: "Appoitment"}}}
+
+      iex> TodoList.update_entry(%TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}, %{id: 2, date: ~D[2018-12-20], title: "Appoitment"})
+      %TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}
+
+  """
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  @spec update_entry(%TodoList{auto_id: number, entries: map}, number, function()) :: %TodoList{
+          auto_id: number,
+          entries: map
+        }
+  @doc """
+  Function to update a entry in the todo list
+
+  ## Examples
+
+      iex> TodoList.update_entry(%TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}, 1, &Map.put(&1, :date, ~D[2018-12-20]))
+      %TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2018-12-20], title: "Appoitment"}}}
+
+      iex> TodoList.update_entry(%TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}, 2, &Map.put(&1, :date, ~D[2018-12-20]))
+      %TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}
+
+  """
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
   @spec entries(%TodoList{auto_id: number, entries: map}, Date) :: [map()]
   @doc """
   Function to get entries by date
 
   ## Examples
 
-      iex> TodoList.entries(%TodoList{auto_id: 3, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, 2 => %{id: 2, date: ~D[2022-01-29], title: "Meeting"}}}, ~D[2022-01-29])
+      iex> TodoList.entries(%TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, 2 => %{id: 2, date: ~D[2022-01-29], title: "Meeting"}}}, ~D[2022-01-29])
       [%{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, %{id: 2, date: ~D[2022-01-29], title: "Meeting"}]
 
       iex> TodoList.entries(%TodoList{auto_id: 3, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, 2 => %{id: 2, date: ~D[2022-01-29], title: "Meeting"}}}, ~D[2022-01-30])
