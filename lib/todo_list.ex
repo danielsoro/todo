29c99d7c48834178d53591669c1171c4b80307b6
Daniel "soro" Cunha <danielsoro@gmail.com>
2022-01-29 11:07:03 -0300

    Introduced struct to my todo list

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,47 +1,59 @@
 defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
   @moduledoc """
   Documentation for `TodoList`.
   This module define a TodoList when you can configure a list of todo by dates
   """
 
-  @spec new :: %{}
+  @spec new :: %TodoList{auto_id: 1, entries: %{}}
   @doc """
   The new function return a new TodoList structure.
 
   ## Examples
 
       iex> TodoList.new()
-      %{}
+      %TodoList{auto_id: 1, entries: %{}}
 
   """
-  def new(), do: MultiDic.new()
+  def new(), do: %TodoList{}
 
-  @spec add_entry(map, %{date: Date, title: String}) :: %{Date => [%{date: Date, title: String}]}
+  @spec add_entry(%TodoList{auto_id: number, entries: map}, map) :: %TodoList{auto_id: number, entries: map}
   @doc """
   Function to add new entry to your todo list.
 
   ## Examples
 
-      iex> TodoList.add_entry(%{}, %{date: ~D[2022-01-29], title: "Appoitment"})
-      %{~D[2022-01-29] => [%{date: ~D[2022-01-29], title: "Appoitment"}]}
+      iex> TodoList.add_entry(%TodoList{}, %{date: ~D[2022-01-29], title: "Appoitment"})
+      %TodoList{auto_id: 2, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}}}
 
   """
   def add_entry(todo_list, entry) do
-    MultiDic.add(todo_list, entry.date, entry)
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list |
+    entries: new_entries,
+    auto_id: todo_list.auto_id + 1
+  }
   end
 
-  @spec entries(map, Date) :: %{Date => %{date: Date, title: String}}
+  @spec entries(%TodoList{auto_id: number, entries: map}, Date) :: [map()]
   @doc """
   Function to get entries by date
 
   ## Examples
 
-      iex> TodoList.entries(%{~D[2022-01-29] => [%{date: ~D[2022-01-29], title: "Appoitment"}, %{date: ~D[2022-01-29], title: "Meeting"}]}, ~D[2022-01-29])
-      [%{date: ~D[2022-01-29], title: "Appoitment"}, %{date: ~D[2022-01-29], title: "Meeting"}]
+      iex> TodoList.entries(%TodoList{auto_id: 3, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, 2 => %{id: 2, date: ~D[2022-01-29], title: "Meeting"}}}, ~D[2022-01-29])
+      [%{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, %{id: 2, date: ~D[2022-01-29], title: "Meeting"}]
 
-      iex> TodoList.entries(%{~D[2022-01-29] => [%{date: ~D[2022-01-29], title: "Appoitment"}]}, ~D[2022-01-30])
-      nil
+      iex> TodoList.entries(%TodoList{auto_id: 3, entries: %{1 => %{id: 1, date: ~D[2022-01-29], title: "Appoitment"}, 2 => %{id: 2, date: ~D[2022-01-29], title: "Meeting"}}}, ~D[2022-01-30])
+      []
 
   """
-  def entries(todo_list, data), do: MultiDic.get(todo_list, data)
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
 end
